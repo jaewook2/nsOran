e2a2b8cb05a8c84c40f2ac8b572628fffc9f1360
Jaewook Lee <iioiioiio123@korea.ac.kr>
2024-07-14 11:28:05 +0900

    Add files via upload

--- poss_snr.py
+++ poss_snr.py
@@ -1,28 +1,65 @@
 import os
 import subprocess
+import numpy as np
+import shutil
+
 # 5초작성
+
+
+def createPoss(nUes, maxX, maxY, path_file = None):
+  possList = np.zeros((nUes,2)) # (x,y point)
+  possIndList = np.random.choice (range(0,(maxX+1)*(maxY+1)), nUes, replace = False)
+  
+  if path_file != None:
+    f = open(path_file,'w')
+  
+  for i in range (nUes):
+    possList[i,0], possList[i,1] = possIndList[i]%maxY, possIndList[i]//maxY
+    if path_file != None:
+    	f.write("%d %d\n"% (possList[i,0], possList[i,1]))
+
+  if path_file != None:
+    f.close()
+    
+  return possList
+
+def createFolder(path_folder):
+  if os.path.isdir (path_folder):
+    shutil.rmtree(path_folder)
+    
+  os.mkdir(path_folder) 
+    
+
+
 maxX = 400
 maxY = 400
-enbP = [[20, 20], [40, 20], [10,37.3205], [10,2.67949]]
+#enbP = [[20, 20], [40, 20], [10,37.3205], [10,2.67949]]
 rootName = 'trace/'
-i_num = 1000
+i_num = 2
+nUes = 12
 # Do iteration
 for i in range (i_num):
-        basePathName = rootName+str(i)
-        # create random possition within (max_X, max_Y)
-        pos_x_list = 
-        pos_y_list = 
-        #poss.txt file 작성 (basePathName)
-        for bs1 in range (2):
-                for bs2 in range (2):
-                        for bs3 in range (2):
-                                for bs4 in range (2):
-                                        if bs1+bs2+bs3+bs3 !=0:
-                                                folderName = str(bs1)+str(bs2)+str(bs3)+str(bs4)
-                                                os.mkdir(folderName)  # 폴더 생성
-                                                pathName = basePathName+'/'+folderName+'/'
-                                                cmd_string_sub = '0513_scenario-zero  --TracePath='+pathName +' --bs1='+str(bs1)+' --bs2='+str(bs2)+' --bs3='+str(bs3)+' --bs4='+str(bs4) + '--poss_file='+basePathName+'/poss.txt'
-                                                # NS-3 상에서 possition file을 읽고 작성
-                                                cmd_string = "./waf --run " +'"'+cmd_string_sub+'"'
-                                                subprocess.Popen(cmd_string, shell=True).wait()
+  basePathName = rootName+str(i)
+  # create_folder
+  createFolder(basePathName) 
+  # create random possition within (max_X, max_Y) and save ue poss to file
+  possList = createPoss(nUes, maxX, maxY, path_file = basePathName+'/ues_poss.txt')
+  
+  for bs1 in range (2):
+    for bs2 in range (2):
+      for bs3 in range (2):
+        for bs4 in range (2):
+          if bs1+bs2+bs3+bs3 !=0:
+            folderName = str(bs1)+str(bs2)+str(bs3)+str(bs4)
+            pathName = basePathName+'/'+folderName
+            createFolder(pathName) # 폴더 생성
+            cmd_string_sub = '0513_scenario-zero --TracePath='+pathName+'/' \
+            + ' --bs1=' + str(bs1) + ' --bs2=' + str(bs2)+ ' --bs3='+str(bs3)+ ' --bs4='+str(bs4) \
+            + ' --poss_file='+basePathName+'/ues_poss.txt'
+
+            # NS-3 상에서 possition file을 읽고 작성
+            cmd_string = "./waf --run " +'"'+cmd_string_sub+'"'
+            subprocess.Popen(cmd_string, shell=True).wait()
+
+
 
